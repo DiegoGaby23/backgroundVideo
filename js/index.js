90f2b9509a03affee49915da9e40e50584b71a29
PEREZ MOTA Javier <gabyperezmota@icloud.com>
2019-03-06 19:17:51 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// Inicio
+
+// Entrada de datos
+let vid = document.getElementById("myvideo");
+// final de las entradas de datos
+
+// proceso
+function playVid() {
+    vid.play(); // impresion a la pantalla
+}
+function pauseVid() {
+    vid.pause(); // impresion a la pantalla
+}
+// final del proceso
+
+// Final
